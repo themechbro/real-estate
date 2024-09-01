5c1b17fb19f0c9eea21ae6959d78e89f78bf8747
themechbro <adrinpaul29@gmail.com>
2024-09-01 23:48:32 +0530

    "Updated Appbar and Drawer components with new styles, layouts, and animations."

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { AppBar, Toolbar } from "@mui/material";
-import useScrollTrigger from "@mui/material/useScrollTrigger";
+import { AppBar, Toolbar, useScrollTrigger } from "@mui/material";
 import { Typography, Button, Box } from "@mui/joy";
 import Badge from "@mui/joy/Badge";
 import { useSelector } from "react-redux";
@@ -8,6 +7,7 @@ import DrawerBasic from "./drawer";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
 
 function ElevationScroll(props) {
   const { children, window } = props;
@@ -16,14 +16,27 @@ function ElevationScroll(props) {
     threshold: 0,
     target: window ? window() : undefined,
   });
-
+  const theme = useTheme();
   return children
     ? React.cloneElement(children, {
-        elevation: trigger ? 4 : 0,
+        sx: {
+          width: trigger ? "100%" : "1200px",
+          position: "fixed",
+          top: 0,
+          left: trigger ? 0 : "calc(50% - 600px)",
+          right: trigger ? 0 : "calc(50% - 600px)",
+          transition: "width 0.3s ease, left 0.3s ease, right 0.3s ease",
+          elevation: trigger ? 4 : 0,
+          [theme.breakpoints.down("md")]: {
+            // Add this media query
+            width: "100%",
+            left: 0,
+            right: 0,
+          },
+        },
       })
     : null;
 }
-
 export default function Appbar(props) {
   const navigate = useNavigate();
   const cart = useSelector((state) => state.view.cart.length);
@@ -35,7 +48,10 @@ export default function Appbar(props) {
     <ElevationScroll {...props}>
       <AppBar
         sx={{
-          backgroundColor: "#fafafa",
+          backgroundColor: "#FFF",
+          padding: "10px 20px",
+          boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
+          color: "#333",
         }}
       >
         <Toolbar
@@ -43,12 +59,18 @@ export default function Appbar(props) {
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
+            alignItems: "center",
           }}
         >
           <Link to="/" style={{ textDecoration: "none" }}>
             <Typography
               level="h6"
-              sx={{ color: "black", fontSize: "2rem", textDecoration: "none" }}
+              sx={{
+                color: "black",
+                fontSize: "2rem",
+                textDecoration: "none",
+                fontWeight: "bold",
+              }}
             >
               Find Home
             </Typography>
@@ -59,23 +81,275 @@ export default function Appbar(props) {
               display: "flex",
               flexDirection: "row",
               justifyContent: "space-evenly",
+              alignItems: "center",
             }}
           >
             {cart ? (
-              <Badge badgeContent={cart}>
+              <Badge
+                badgeContent={cart}
+                sx={{
+                  "& .MuiBadge-badge": {
+                    backgroundColor: "#333",
+                    color: "#fff",
+                    fontSize: "12px",
+                    padding: "4px 8px",
+                    borderRadius: "10px",
+                  },
+                }}
+              >
                 <DrawerBasic />
               </Badge>
             ) : (
-              <Button variant="plain" startDecorator={<ShoppingCartIcon />}>
+              <Button
+                variant="plain"
+                startDecorator={<ShoppingCartIcon />}
+                sx={{
+                  color: "#333",
+                  fontSize: "16px",
+                  textTransform: "none",
+                }}
+              >
                 Cart
               </Button>
             )}
 
-            <Button>Login</Button>
-            <Button>Sign Up</Button>
+            <Button
+              variant="outlined"
+              sx={{
+                color: "#333",
+                fontSize: "16px",
+                textTransform: "none",
+                borderColor: "#333",
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              sx={{
+                color: "#fff",
+                fontSize: "16px",
+                textTransform: "none",
+                backgroundColor: "#333",
+              }}
+            >
+              Sign Up
+            </Button>
           </Box>
         </Toolbar>
       </AppBar>
     </ElevationScroll>
   );
 }
+
+// import * as React from "react";
+// import { AppBar, Toolbar } from "@mui/material";
+// import useScrollTrigger from "@mui/material/useScrollTrigger";
+// import { Typography, Button, Box } from "@mui/joy";
+// import Badge from "@mui/joy/Badge";
+// import { useSelector } from "react-redux";
+// import DrawerBasic from "./drawer";
+// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+// import { useNavigate } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+// function ElevationScroll(props) {
+//   const { children, window } = props;
+//   const trigger = useScrollTrigger({
+//     disableHysteresis: true,
+//     threshold: 0,
+//     target: window ? window() : undefined,
+//   });
+
+//   return children
+//     ? React.cloneElement(children, {
+//         elevation: trigger ? 4 : 0,
+//       })
+//     : null;
+// }
+
+// export default function Appbar(props) {
+//   const navigate = useNavigate();
+//   const cart = useSelector((state) => state.view.cart.length);
+//   console.log(cart);
+
+//   const [open1, setOpen1] = React.useState(false);
+
+//   return (
+//     <ElevationScroll {...props}>
+//       <AppBar
+//         sx={{
+//           backgroundColor: "#fafafa",
+//           padding: "10px 20px",
+//           boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
+//         }}
+//       >
+//         <Toolbar
+//           sx={{
+//             display: "flex",
+//             flexDirection: "row",
+//             justifyContent: "space-between",
+//             alignItems: "center",
+//           }}
+//         >
+//           <Link to="/" style={{ textDecoration: "none" }}>
+//             <Typography
+//               level="h6"
+//               sx={{
+//                 color: "black",
+//                 fontSize: "2rem",
+//                 textDecoration: "none",
+//                 fontWeight: "bold",
+//               }}
+//             >
+//               Find Home
+//             </Typography>
+//           </Link>
+//           <Box
+//             sx={{
+//               width: "500px",
+//               display: "flex",
+//               flexDirection: "row",
+//               justifyContent: "space-evenly",
+//               alignItems: "center",
+//             }}
+//           >
+//             {cart ? (
+//               <Badge
+//                 badgeContent={cart}
+//                 sx={{
+//                   "& .MuiBadge-badge": {
+//                     backgroundColor: "#333",
+//                     color: "#fff",
+//                     fontSize: "12px",
+//                     padding: "4px 8px",
+//                     borderRadius: "10px",
+//                   },
+//                 }}
+//               >
+//                 <DrawerBasic />
+//               </Badge>
+//             ) : (
+//               <Button
+//                 variant="plain"
+//                 startDecorator={<ShoppingCartIcon />}
+//                 sx={{
+//                   color: "#333",
+//                   fontSize: "16px",
+//                   textTransform: "none",
+//                 }}
+//               >
+//                 Cart
+//               </Button>
+//             )}
+
+//             <Button
+//               variant="outlined"
+//               sx={{
+//                 color: "#333",
+//                 fontSize: "16px",
+//                 textTransform: "none",
+//                 borderColor: "#333",
+//               }}
+//             >
+//               Login
+//             </Button>
+//             <Button
+//               variant="contained"
+//               sx={{
+//                 color: "#fff",
+//                 fontSize: "16px",
+//                 textTransform: "none",
+//                 backgroundColor: "#333",
+//               }}
+//             >
+//               Sign Up
+//             </Button>
+//           </Box>
+//         </Toolbar>
+//       </AppBar>
+//     </ElevationScroll>
+//   );
+// }
+
+// import * as React from "react";
+// import { AppBar, Toolbar } from "@mui/material";
+// import useScrollTrigger from "@mui/material/useScrollTrigger";
+// import { Typography, Button, Box } from "@mui/joy";
+// import Badge from "@mui/joy/Badge";
+// import { useSelector } from "react-redux";
+// import DrawerBasic from "./drawer";
+// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+// import { useNavigate } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+// function ElevationScroll(props) {
+//   const { children, window } = props;
+//   const trigger = useScrollTrigger({
+//     disableHysteresis: true,
+//     threshold: 0,
+//     target: window ? window() : undefined,
+//   });
+
+//   return children
+//     ? React.cloneElement(children, {
+//         elevation: trigger ? 4 : 0,
+//       })
+//     : null;
+// }
+
+// export default function Appbar(props) {
+//   const navigate = useNavigate();
+//   const cart = useSelector((state) => state.view.cart.length);
+//   console.log(cart);
+
+//   const [open1, setOpen1] = React.useState(false);
+
+//   return (
+//     <ElevationScroll {...props}>
+//       <AppBar
+//         sx={{
+//           backgroundColor: "#fafafa",
+//         }}
+//       >
+//         <Toolbar
+//           sx={{
+//             display: "flex",
+//             flexDirection: "row",
+//             justifyContent: "space-between",
+//           }}
+//         >
+//           <Link to="/" style={{ textDecoration: "none" }}>
+//             <Typography
+//               level="h6"
+//               sx={{ color: "black", fontSize: "2rem", textDecoration: "none" }}
+//             >
+//               Find Home
+//             </Typography>
+//           </Link>
+//           <Box
+//             sx={{
+//               width: "500px",
+//               display: "flex",
+//               flexDirection: "row",
+//               justifyContent: "space-evenly",
+//             }}
+//           >
+//             {cart ? (
+//               <Badge badgeContent={cart}>
+//                 <DrawerBasic />
+//               </Badge>
+//             ) : (
+//               <Button variant="plain" startDecorator={<ShoppingCartIcon />}>
+//                 Cart
+//               </Button>
+//             )}
+
+//             <Button>Login</Button>
+//             <Button>Sign Up</Button>
+//           </Box>
+//         </Toolbar>
+//       </AppBar>
+//     </ElevationScroll>
+//   );
+// }
--- src/components/drawer.js
+++ src/components/drawer.js
@@ -13,7 +13,7 @@ import { useNavigate } from "react-router-dom";
 import ListItemContent from "@mui/joy/ListItemContent";
 import ListItemDecorator from "@mui/joy/ListItemDecorator";
 import Typography from "@mui/joy/Typography";
-import { Paper } from "@mui/material";
+import { Paper, Grid, Card, Fade, Slide } from "@mui/material";
 
 export default function DrawerBasic() {
   const navigate = useNavigate();
@@ -38,6 +38,18 @@ export default function DrawerBasic() {
         variant="outlined"
         color="neutral"
         onClick={toggleDrawer(true)}
+        sx={{
+          borderRadius: "50px",
+          padding: "10px 20px",
+          fontSize: "18px",
+          fontWeight: "bold",
+          transition: "all 0.3s ease",
+          "&:hover": {
+            backgroundColor: "#f9f9f9",
+            color: "#333",
+            animation: "bounce 0.5s ease",
+          },
+        }}
       >
         Cart
       </Button>
@@ -45,55 +57,225 @@ export default function DrawerBasic() {
         open={open}
         onClose={toggleDrawer(false)}
         anchor="right"
-        size="lg"
+        size="md"
+        sx={{
+          "& .MuiDrawer-paper": {
+            background: "#f9f9f9",
+            padding: "20px",
+            width: "400px",
+            transition: "all 0.3s ease",
+          },
+        }}
       >
-        <Box
-          role="presentation"
-          onClick={toggleDrawer(false)}
-          onKeyDown={toggleDrawer(false)}
-        >
-          <Typography
-            level="h1"
-            className="text-center"
-            sx={{ padding: "1rem" }}
+        <Slide direction="right" in={open} mountOnEnter unmountOnExit>
+          <Box
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
           >
-            Your Cart ({cart.length}){" "}
-          </Typography>
-          <List sx={{ marginBottom: 5 }}>
-            {cart.map((i) => (
-              <div>
-                <Paper elevation={4} sx={{ padding: 1, marginBottom: 2 }}>
-                  <ListItem sx={{ padding: 2 }}>
-                    <ListItemDecorator>
-                      <Avatar size="lg" src={i.image} sx={{ marginRight: 1 }} />
-                    </ListItemDecorator>
-                    <Typography level="title-lg">{i.name}</Typography>
-                  </ListItem>
-                  <ListItem>
-                    <Typography level="body-lg">{i.description}</Typography>
-                  </ListItem>
-                  <ListDivider inset="gutter" />
-                  <Typography level="body-lg">$ {i.price}</Typography>
-                </Paper>
-              </div>
-            ))}
-          </List>
-          <Divider
-            sx={{
-              marginBottom: 5,
-            }}
-          />
-          <Button
-            variant="outlined"
-            color="success"
-            sx={{ width: "100%", marginTop: "10%" }}
-            level="body-lg"
-            onClick={() => navigate("/checkout")}
-          >
-            Proceed to Buy
-          </Button>
-        </Box>
+            <Fade in={open} timeout={500}>
+              <Typography
+                level="h1"
+                className="text-center"
+                sx={{
+                  padding: "1rem",
+                  fontSize: "24px",
+                  fontWeight: "bold",
+                  color: "#333",
+                }}
+              >
+                Your Cart ({cart.length})
+              </Typography>
+            </Fade>
+            <Grid container spacing={2}>
+              {cart.map((i) => (
+                <Grid item xs={12} key={i.id}>
+                  <Fade in={open} timeout={500}>
+                    <Card
+                      variant="outlined"
+                      sx={{
+                        padding: "20px",
+                        borderRadius: "20px",
+                        boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
+                        transition: "all 0.3s ease",
+                        "&:hover": {
+                          boxShadow: "0px 0px 20px rgba(0,0,0,0.2)",
+                          transform: "scale(1.05)",
+                        },
+                      }}
+                    >
+                      <Grid container spacing={2}>
+                        <Grid item xs={4}>
+                          <Avatar
+                            size="lg"
+                            src={i.image}
+                            sx={{
+                              borderRadius: "10px",
+                              width: "100%",
+                              height: "100%",
+                              "&:hover": {
+                                transform: "rotate(10deg)",
+                              },
+                            }}
+                          />
+                        </Grid>
+                        <Grid item xs={8}>
+                          <Typography
+                            level="title-lg"
+                            sx={{
+                              fontSize: "18px",
+                              fontWeight: "bold",
+                              color: "#333",
+                            }}
+                          >
+                            {i.name}
+                          </Typography>
+                          <Typography
+                            level="body-lg"
+                            sx={{
+                              fontSize: "14px",
+                              color: "#666",
+                            }}
+                          >
+                            {i.description}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                      <Typography
+                        level="body-lg"
+                        sx={{
+                          fontSize: "16px",
+                          fontWeight: "bold",
+                          color: "#666",
+                          marginTop: "10px",
+                        }}
+                      >
+                        $ {i.price}
+                      </Typography>
+                    </Card>
+                  </Fade>
+                </Grid>
+              ))}
+            </Grid>
+            <Divider
+              sx={{
+                marginBottom: 5,
+              }}
+            />
+            <Fade in={open} timeout={500}>
+              <Button
+                variant="contained"
+                color="success"
+                sx={{ width: "100%", marginTop: "10%" }}
+                level="body-lg"
+                onClick={() => navigate("/checkout")}
+              >
+                Proceed to Buy
+              </Button>
+            </Fade>
+          </Box>
+        </Slide>
       </Drawer>
     </Box>
   );
 }
+
+// import * as React from "react";
+// import Box from "@mui/joy/Box";
+// import Drawer from "@mui/joy/Drawer";
+// import Button from "@mui/joy/Button";
+// import List from "@mui/joy/List";
+// import Divider from "@mui/joy/Divider";
+// import ListItem from "@mui/joy/ListItem";
+// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+// import { useSelector } from "react-redux";
+// import Avatar from "@mui/joy/Avatar";
+// import ListDivider from "@mui/joy/ListDivider";
+// import { useNavigate } from "react-router-dom";
+// import ListItemContent from "@mui/joy/ListItemContent";
+// import ListItemDecorator from "@mui/joy/ListItemDecorator";
+// import Typography from "@mui/joy/Typography";
+// import { Paper } from "@mui/material";
+
+// export default function DrawerBasic() {
+//   const navigate = useNavigate();
+//   const cart = useSelector((state) => state.view.cart);
+//   const [open, setOpen] = React.useState(false);
+
+//   const toggleDrawer = (inOpen) => (event) => {
+//     if (
+//       event.type === "keydown" &&
+//       (event.key === "Tab" || event.key === "Shift")
+//     ) {
+//       return;
+//     }
+
+//     setOpen(inOpen);
+//   };
+
+//   return (
+//     <Box sx={{ display: "flex" }}>
+//       <Button
+//         startDecorator={<ShoppingCartIcon />}
+//         variant="outlined"
+//         color="neutral"
+//         onClick={toggleDrawer(true)}
+//       >
+//         Cart
+//       </Button>
+//       <Drawer
+//         open={open}
+//         onClose={toggleDrawer(false)}
+//         anchor="right"
+//         size="lg"
+//       >
+//         <Box
+//           role="presentation"
+//           onClick={toggleDrawer(false)}
+//           onKeyDown={toggleDrawer(false)}
+//         >
+//           <Typography
+//             level="h1"
+//             className="text-center"
+//             sx={{ padding: "1rem" }}
+//           >
+//             Your Cart ({cart.length}){" "}
+//           </Typography>
+//           <List sx={{ marginBottom: 5 }}>
+//             {cart.map((i) => (
+//               <div>
+//                 <Paper elevation={4} sx={{ padding: 1, marginBottom: 2 }}>
+//                   <ListItem sx={{ padding: 2 }}>
+//                     <ListItemDecorator>
+//                       <Avatar size="lg" src={i.image} sx={{ marginRight: 1 }} />
+//                     </ListItemDecorator>
+//                     <Typography level="title-lg">{i.name}</Typography>
+//                   </ListItem>
+//                   <ListItem>
+//                     <Typography level="body-lg">{i.description}</Typography>
+//                   </ListItem>
+//                   <ListDivider inset="gutter" />
+//                   <Typography level="body-lg">$ {i.price}</Typography>
+//                 </Paper>
+//               </div>
+//             ))}
+//           </List>
+//           <Divider
+//             sx={{
+//               marginBottom: 5,
+//             }}
+//           />
+//           <Button
+//             variant="outlined"
+//             color="success"
+//             sx={{ width: "100%", marginTop: "10%" }}
+//             level="body-lg"
+//             onClick={() => navigate("/checkout")}
+//           >
+//             Proceed to Buy
+//           </Button>
+//         </Box>
+//       </Drawer>
+//     </Box>
+//   );
+// }
